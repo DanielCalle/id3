b5646762e10738b4a3f7c11b4174be4117fe8ace
Daniel Calle <danielcallesanchez@gmail.com>
2019-03-18 16:31:17 +0100

    Read keys

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 class ID3:
     def run(self):
-        print("Hello, world!")
+        filepath = "AtributosJuego.txt"
+        with open(filepath) as fp:
+            lines = fp.read().splitlines()
+            keys = lines[0].split(",")
+            print(keys)
+        
+        
 
 
 if __name__ == '__main__':
