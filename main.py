ee404e5a183d44bc1d7748ca0f49e2775a32c753
Daniel Calle <danielcallesanchez@gmail.com>
2019-03-19 04:10:17 +0100

    Using Pandas

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
+import pandas as pd
+
+
 class ID3:
     def run(self):
+        keys = pd.read_csv('AtributosJuego.txt', sep=",", header=None)
+        data = pd.read_csv('Juego.txt', sep=",", header=None)
+        """
         filepath = "AtributosJuego.txt"
         with open(filepath) as fp:
             lines = fp.read().splitlines()
             keys = lines[0].split(",")
-            print(keys)
+
+        filepath = "Juego.txt"
+        with open(filepath) as fp:
+            lines = fp.read().splitlines()
+            for line in lines:
+                data.append(line.split(","))
+        """
         
-        
+        print(keys)
+        print(data)
 
 
 if __name__ == '__main__':
